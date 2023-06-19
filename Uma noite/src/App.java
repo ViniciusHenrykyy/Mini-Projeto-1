ddd0dd84a3c93d58857ebabffbc86d4e4ce33e44
Vinicius Henrykyy Henkenskk Carvalho Gomes <viniciushenrykyy@gmail.com>
2023-06-19 17:30:51 -0300

    eu escrevi a primeira parte da história: "Uma Noite".

--- Uma noite/src/App.java
+++ Uma noite/src/App.java
@@ -1,5 +1,53 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(
+                "Era meia noite e meia, era inverno e estava frio lá fora, eu estava no meu quarto como de costume, com a mesma insônia imensurável de sempre");
+        System.out.println(
+                "Mais naquele dia em específico, eu abri a janela do meu quarto, que tinha vista para uma paisagem branca e pálida, iluminada pela lua que estava muito bonita naquela noite.");
+        System.out.println(
+                "Eu resolvi pensar sobre a vida, e lembrar das muitas amizades que eu perdi contato na infância. Eram muitas, e eu parei pra pensar o que eles estão fazendo agora?, quais estão ");
+        System.out.println("sendo seus sonhos seus objetivos de vida, suas conquistas?");
+        System.out.println(
+                "Bom essa resposta eu só iria obter se eu ainda mantivesse contato com essas pessoas, mais com o passar dos anos, caminhos diferentes, pessoas novas que a gente vai conhecendo, parece que a gente perde interesse nas pessoas com quem já convivemos.");
+        System.out.println(
+                "Nessa noite eu lembrei dela, ela que me ajudou tanto a superar meus medos, e enfrentar de frente quem me oprimia, e me fez pensar sobre como que eu consegui esquecer de alguém que fez tanto por mim, como eu me esqueci?");
+
+        System.out.println(
+                "No ensino fundamental eu não tinha amigos eu era gordo, tímido e não tinha coragem de interagir com ninguém.");
+
+        System.out.println(
+                "Eu estava no 4 ano e frequentemente eu sofria bullying de uns garotos do 7 ano, eles me batiam, me chutavam me xingavam, e eu não tinha forças pra fazer nada.");
+
+        System.out.println(
+                "Foi aí que eu conheci ela, jully que era uma garota gentil e carismática, ela se dava bem com todos inclusive com os valentões que me agrediam.");
+
+        System.out.println(
+                "O dia em que eu conheci ela foi quando os valentões me levaram pra trás da escola e me deram uma surra por não ter feito algo que eles me pediram. Ela viu tudo e entregou eles para o diretor, que suspendeu os três. Ela me tranquilizou e estendeu a mão para mim.");
+
+        System.out.println(
+                "A partir daí eu comecei a andar com a jully, a gente lanchava junto, conversava e pela primeira vez eu tinha encontrado uma amiga. Ela me ajudou muito a superar minhas inseguranças e a terminar o ensino fundamental sem ficar sozinho.");
+
+        System.out.println(
+                "Quando eu cheguei no 8 ano, ela tinha saído da escola pois ia se mudar para outro estado, eu fiquei muito triste, porque minha melhor amiga ia embora.");
+        System.out.println(
+                "Ela me tranquilizou pois me mostrou o quanto de amizades eu já tinha feito nesses 4 anos, eu agradeci a ela e disse pra gente manter contato pois eu não queria deixar de falar com ela.");
+
+        System.out.println(
+                "Agora depois de ter mudado tanto, de ter amadurecido o suficiente eu parei pra pensar que na vida adulta a gente acaba esquecendo daqueles que ajudaram você a se tornar o que você é hoje.");
+
+        System.out.println(
+                "Então naquela noite tendo isso em mente eu procurei por ela, seja no Instagram, Facebook, eu estava destinado a encontrar ela, eu precisava conversar com ela, eu precisava saber como ela estava, eu precisava ouvir a voz dela.");
+
+        System.out.println(
+                "Eram duas horas da manhã e eu já estava quase perdendo as esperanças quando eu encontrei uma menina que era muita amiga da jully naquela época.");
+
+        System.out.println(
+                "Entrando no perfil do Facebook dela eu encontrei o perfil da jully e eu mandei uma mensagem pra ela, 'lembra de mim sou eu Kyle, você tem um tempo pra conversar?'.");
+
+        System.out.println(
+                "Não esperava uma resposta imediata mais nem foi isso que me surpreendeu, foi a resposta dela.");
+
+        System.out.println(
+                "'Desculpa mais eu não quero conversar com ninguém',... dentro de mim surgiu um sentimento imensurável de ligar para ela. Mais eu não sabia se era certo depois doque ela me disse.");
     }
 }
